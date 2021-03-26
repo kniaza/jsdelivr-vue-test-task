bb56e52674bf5e9a3e77c08d3ae727a8656b08d6
kniaza <gurami.niazashvili@gmail.com>
2021-03-26 17:24:06 +0400

    add comman components

---
+++ src/components/common/FileTree.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="file in files" :key="file.name">
+      <span>
+        <BIconFolder v-if="file.type === 'directory'" />
+        <BIconFileEarmarkCode v-if="file.type === 'file'" />
+      </span>
+      <span class="name">{{ file.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BIconFileEarmarkCode, BIconFolder } from 'bootstrap-vue'
+export default {
+  components: { BIconFileEarmarkCode, BIconFolder },
+  props: {
+    files: Array,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.name {
+  display: inline-block;
+  padding-left: 1rem;
+}
+</style>
---
+++ src/components/common/Tags.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="tags">
+      <span
+        :class="{ tag: true, clickable }"
+        v-for="(tag, index) in tags"
+        :key="index"
+        @click="$emit('click-tag', tag)"
+      >
+        <span>{{ tag }}</span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tags: Array,
+    clickable: Boolean,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  display: flex;
+  flex-wrap: wrap;
+  .tag {
+    border: 1px solid #cccccc50;
+    background: #cccccc30;
+    border-radius: 10px;
+    padding: 0.3rem 0.7rem;
+    margin-right: 0.5rem;
+    margin-bottom: 0.5rem;
+    font-size: 0.8rem;
+    &.clickable {
+      transition: 0.2s ease-in;
+      &:hover {
+        border-color: var(--blue) !important;
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
